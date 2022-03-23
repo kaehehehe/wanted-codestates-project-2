4ed26b081868dd2c50c0496a607ec6dfafef2e94
Kae <kaehehehe@gmail.com>
2022-03-23 20:18:01 +0900

    :sparkles: 리타이어 제외 기능 구현

--- src/pages/User/User.jsx
+++ src/pages/User/User.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from 'react';
+import React, { useState, useContext, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 
 import * as S from './style';
@@ -9,11 +9,16 @@ import { convertDataIntoAgo } from '../../utils/convertDataIntoAgo';
 import { convertDataIntoTime } from '../../utils/convertDataIntoTime';
 import { track as TRACK_DATA } from '../../assets/metadata/track';
 import { kart as KART_DATA } from '../../assets/metadata/kart';
+import Toggle from '../../components/Toggle/Toggle';
 
 const User = () => {
   const [matchType, setMatchType] = useState('solo');
   const { nickname } = useParams();
   const { useSoloMatchData, useTeamMatchData } = useContext(GlobalContext);
+
+  const [removeRetire, setRemoveRetire] = useState(false);
+  const [removeRetireSoloData, setRetireSoloData] = useState([]);
+  const [removeRetireTeamData, setRetireTeamData] = useState([]);
 
   const findTrackName = (trackId) => {
     const track = TRACK_DATA.filter((item) => item.id === trackId);
@@ -25,6 +30,23 @@ const User = () => {
     return track[0].name;
   };
 
+  const removeRetireMatch = (data) => {
+    const result = data.filter((item) => item.player.matchRetired !== '1');
+    return result;
+  };
+
+  useEffect(() => {
+    if (useSoloMatchData) {
+      const result = removeRetireMatch(useSoloMatchData);
+      setRetireSoloData(result);
+    }
+
+    if (useTeamMatchData) {
+      const result = removeRetireMatch(useTeamMatchData);
+      setRetireTeamData(result);
+    }
+  }, []);
+
   return (
     <>
       {useSoloMatchData && useTeamMatchData && (
@@ -34,30 +56,64 @@ const User = () => {
             setMatchType={setMatchType}
             nickname={nickname}
           />
-          {matchType === 'solo' &&
-            useSoloMatchData.map((data) => (
-              <MatchRecord
-                key={data.matchId}
-                endTime={convertDataIntoAgo(data.endTime)}
-                rank={data.player.matchRank}
-                playerCount={data.playerCount}
-                track={findTrackName(data.trackId)}
-                kart={findKartName(data.player.kart)}
-                matchTime={convertDataIntoTime(data.player.matchTime)}
-              />
-            ))}
-          {matchType === 'team' &&
-            useTeamMatchData.map((data) => (
-              <MatchRecord
-                key={data.matchId}
-                endTime={convertDataIntoAgo(data.endTime)}
-                rank={data.player.matchRank}
-                playerCount={data.playerCount}
-                track={findTrackName(data.trackId)}
-                kart={findKartName(data.player.kart)}
-                matchTime={convertDataIntoTime(data.player.matchTime)}
-              />
-            ))}
+          <S.MatchRecordList>
+            <S.ToggleWrapper>
+              <p>리타이어 제외</p>
+              <Toggle show={removeRetire} setShow={setRemoveRetire} />
+            </S.ToggleWrapper>
+            {matchType === 'solo' &&
+              removeRetire &&
+              removeRetireSoloData.map((data) => (
+                <MatchRecord
+                  key={data.matchId}
+                  endTime={convertDataIntoAgo(data.endTime)}
+                  rank={data.player.matchRank}
+                  playerCount={data.playerCount}
+                  track={findTrackName(data.trackId)}
+                  kart={findKartName(data.player.kart)}
+                  matchTime={convertDataIntoTime(data.player.matchTime)}
+                />
+              ))}
+            {matchType === 'solo' &&
+              !removeRetire &&
+              useSoloMatchData.map((data) => (
+                <MatchRecord
+                  key={data.matchId}
+                  endTime={convertDataIntoAgo(data.endTime)}
+                  rank={data.player.matchRank}
+                  playerCount={data.playerCount}
+                  track={findTrackName(data.trackId)}
+                  kart={findKartName(data.player.kart)}
+                  matchTime={convertDataIntoTime(data.player.matchTime)}
+                />
+              ))}
+            {matchType === 'team' &&
+              removeRetire &&
+              removeRetireTeamData.map((data) => (
+                <MatchRecord
+                  key={data.matchId}
+                  endTime={convertDataIntoAgo(data.endTime)}
+                  rank={data.player.matchRank}
+                  playerCount={data.playerCount}
+                  track={findTrackName(data.trackId)}
+                  kart={findKartName(data.player.kart)}
+                  matchTime={convertDataIntoTime(data.player.matchTime)}
+                />
+              ))}
+            {matchType === 'team' &&
+              !removeRetire &&
+              useTeamMatchData.map((data) => (
+                <MatchRecord
+                  key={data.matchId}
+                  endTime={convertDataIntoAgo(data.endTime)}
+                  rank={data.player.matchRank}
+                  playerCount={data.playerCount}
+                  track={findTrackName(data.trackId)}
+                  kart={findKartName(data.player.kart)}
+                  matchTime={convertDataIntoTime(data.player.matchTime)}
+                />
+              ))}
+          </S.MatchRecordList>
         </S.Container>
       )}
     </>
--- src/pages/User/style.js
+++ src/pages/User/style.js
@@ -4,3 +4,22 @@ export const Container = styled.div`
   max-width: 1100px;
   margin: 0 auto;
 `;
+
+export const MatchRecordList = styled.div`
+  position: relative;
+  margin-top: 50px;
+`;
+
+export const ToggleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  position: absolute;
+  top: -35px;
+  right: 15px;
+
+  p {
+    font-size: 16px;
+    margin-right: 15px;
+    color: ${({ theme }) => theme.mainColor};
+  }
+`;
