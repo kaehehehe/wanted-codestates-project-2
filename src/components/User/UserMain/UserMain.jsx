0e1805a157dbf3d2ec973ba94db0b9ac5b441834
Kae <kaehehehe@gmail.com>
2022-03-24 03:09:38 +0900

    :sparkles: 유저에 따라 캐릭터 이미지가 바뀌도록 설정

--- src/components/User/UserMain/UserMain.jsx
+++ src/components/User/UserMain/UserMain.jsx
@@ -1,19 +1,21 @@
 import React from 'react';
-import Character from '../../../assets/metadata/character/42c729e64e31aea803e4881432f7b95129ce97535c29e4f9a72919a9f267b418.png';
 
 import * as S from './style';
 
-const Header = ({ matchType, setMatchType, nickname }) => {
+const Header = ({ matchType, setMatchType, nickname, imgId }) => {
   return (
     <S.Header>
-      <S.Character src={Character} alt="character" />
+      <S.Character
+        src={`https://s3-ap-northeast-1.amazonaws.com/solution-userstats/metadata/character/${imgId}.png`}
+        alt="character"
+      />
       <S.Wrapper>
         <S.Nickname>{nickname}</S.Nickname>
         <S.Buttons>
-          <S.SoloBtn matchType={matchType} onClick={()=>setMatchType('solo')}>
+          <S.SoloBtn matchType={matchType} onClick={() => setMatchType('solo')}>
             개인전
           </S.SoloBtn>
-          <S.TeamBtn matchType={matchType} onClick={()=>setMatchType('team')}>
+          <S.TeamBtn matchType={matchType} onClick={() => setMatchType('team')}>
             팀전
           </S.TeamBtn>
         </S.Buttons>
