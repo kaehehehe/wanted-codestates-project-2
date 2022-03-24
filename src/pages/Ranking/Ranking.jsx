e4c0b323eb093269c44aabaa738cb441a69cb208
Kae <kaehehehe@gmail.com>
2022-03-25 00:49:32 +0900

    :lipstick: wave 애니메이션 추가

--- src/pages/Ranking/Ranking.jsx
+++ src/pages/Ranking/Ranking.jsx
@@ -14,45 +14,68 @@ const Ranking = () => {
   const [matchCourse, setMatchCourse] = useState('통합');
 
   return (
-    <S.Container>
-      <S.ButtonWrapper>
-        <S.MatchTypeBtn>
-          <MatchTypeBtn
-            matchType={matchType}
-            setMatchType={setMatchType}
-            text_1="개인전"
-            text_2="팀전"
-          />
-        </S.MatchTypeBtn>
-        <MatchTypeBtn
-          matchType={matchCourse}
-          setMatchType={setMatchCourse}
-          text_1="통합"
-          text_2="무한부스터"
-        />
-      </S.ButtonWrapper>
-      <S.CardWrapper>
-        {matchType === '개인전' &&
-          matchCourse === '통합' &&
-          speedIndiCombineRanking.map((item, index) => {
-            if (index <= 2) return <Card key={item.nickname} data={item} />;
-          })}
-        {matchType === '개인전' &&
-          matchCourse === '무한부스터' &&
-          speedIndiInfinitRanking.map((item, index) => {
-            if (index <= 2) return <Card key={item.nickname} data={item} />;
-          })}
-        {matchType === '팀전' &&
-          matchCourse === '통합' &&
-          speedTeamCombineRanking.map((item, index) => {
-            if (index <= 2) return <Card key={item.nickname} data={item} />;
-          })}
-        {matchType === '팀전' &&
-          matchCourse === '무한부스터' &&
-          speedTeamInfinitRanking.map((item, index) => {
-            if (index <= 2) return <Card key={item.nickname} data={item} />;
-          })}
-      </S.CardWrapper>
+    <>
+      <S.Background>
+        <S.Wrapper>
+          <S.ButtonWrapper>
+            <S.MatchTypeBtn>
+              <MatchTypeBtn
+                matchType={matchType}
+                setMatchType={setMatchType}
+                text_1="개인전"
+                text_2="팀전"
+              />
+            </S.MatchTypeBtn>
+            <MatchTypeBtn
+              matchType={matchCourse}
+              setMatchType={setMatchCourse}
+              text_1="통합"
+              text_2="무한부스터"
+            />
+          </S.ButtonWrapper>
+          <S.CardWrapper>
+            {matchType === '개인전' &&
+              matchCourse === '통합' &&
+              speedIndiCombineRanking.map((item, index) => {
+                if (index <= 2) return <Card key={item.nickname} data={item} />;
+              })}
+            {matchType === '개인전' &&
+              matchCourse === '무한부스터' &&
+              speedIndiInfinitRanking.map((item, index) => {
+                if (index <= 2) return <Card key={item.nickname} data={item} />;
+              })}
+            {matchType === '팀전' &&
+              matchCourse === '통합' &&
+              speedTeamCombineRanking.map((item, index) => {
+                if (index <= 2) return <Card key={item.nickname} data={item} />;
+              })}
+            {matchType === '팀전' &&
+              matchCourse === '무한부스터' &&
+              speedTeamInfinitRanking.map((item, index) => {
+                if (index <= 2) return <Card key={item.nickname} data={item} />;
+              })}
+          </S.CardWrapper>
+        </S.Wrapper>
+        <svg
+          className="waves"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlnsXlink="http://www.w3.org/1999/xlink"
+          viewBox="0 24 150 28"
+          preserveAspectRatio="none"
+          shape-rendering="auto"
+        >
+          <defs>
+            <path
+              id="gentle-wave"
+              d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+            />
+          </defs>
+          <g className="parallax">
+            <use xlinkHref="#gentle-wave" x="48" y="5" fill="#016AE4" />
+            <use xlinkHref="#gentle-wave" x="48" y="5" fill="#016FED" />
+          </g>
+        </svg>
+      </S.Background>
       <S.ListWrapper>
         {matchType === '개인전' &&
           matchCourse === '통합' &&
@@ -75,7 +98,7 @@ const Ranking = () => {
             if (index >= 3) return <List key={item.nickname} data={item} />;
           })}
       </S.ListWrapper>
-    </S.Container>
+    </>
   );
 };
 
--- src/pages/Ranking/style.js
+++ src/pages/Ranking/style.js
@@ -1,13 +1,51 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-export const Container = styled.div`
-  max-width: 1100px;
-  margin: 0 auto;
+const wave = keyframes`
+  0% {
+  transform: translate3d(-90px,0,0);
+  }
+  100% { 
+    transform: translate3d(85px,0,0);
+  }
+`;
+
+export const Background = styled.div`
+  width: 100vw;
+  background-color: #015ecc;
+  position: relative;
+
+  .waves {
+    margin-top: 150px;
+  }
+
+  .parallax > use {
+    animation: ${wave} 25s cubic-bezier(0.55, 0.5, 0.45, 0.5) infinite;
+  }
+
+  .parallax > use:nth-child(1) {
+    animation-delay: -5s;
+    animation-duration: 8s;
+  }
+
+  .parallax > use:nth-child(2) {
+    animation-delay: -8s;
+    animation-duration: 5s;
+  }
+`;
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translateY(-50%) translateX(-50%);
 `;
 
 export const ButtonWrapper = styled.div`
+width: 100%;
   display: flex;
-  padding-left: 10px;
+  justify-content: flex-start;
   margin-bottom: 30px;
 `;
 
@@ -19,10 +57,13 @@ export const CardWrapper = styled.ul`
   display: flex;
   justify-content: center;
   align-items: center;
-  flex-wrap: wrap;
+  width: 100%;
 `;
 
 export const ListWrapper = styled.ul`
+  max-width: 1100px;
+  margin: 0 auto;
   display: flex;
   flex-direction: column;
+  margin-top: 20px;
 `;
