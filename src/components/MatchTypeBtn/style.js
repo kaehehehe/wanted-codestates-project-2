51a0209ea2cde322c8e842d31047eca2228c147d
Kae <kaehehehe@gmail.com>
2022-03-25 00:17:59 +0900

    :lipstick: 컬러 변경

--- src/components/MatchTypeBtn/style.js
+++ src/components/MatchTypeBtn/style.js
@@ -7,15 +7,15 @@ export const Buttons = styled.div`
 export const LeftBtn = styled.button`
   width: 100px;
   padding: 5px 0;
-  border: 1px solid #015ecc;
+  border: 1px solid #003297;
   border-radius: 5px 0 0 5px;
-  background-color: ${({ active }) => (active ? '#015ecc' : '#FFFFFF')};
-  color: ${({ active }) => (active ? '#FFFFFF' : '#015ecc')};
+  background-color: ${({ active }) => (active ? '#003297' : '#FFFFFF')};
+  color: ${({ active }) => (active ? '#FFFFFF' : '#003297')};
   cursor: pointer;
 `;
 
 export const RightBtn = styled(LeftBtn)`
   border-radius: 0 5px 5px 0;
-  background-color: ${({ active }) => (active ? '#015ecc' : '#FFFFFF')};
-  color: ${({ active }) => (active ? '#FFFFFF' : '#015ecc')};
+  background-color: ${({ active }) => (active ? '#003297' : '#FFFFFF')};
+  color: ${({ active }) => (active ? '#FFFFFF' : '#003297')};
 `;
