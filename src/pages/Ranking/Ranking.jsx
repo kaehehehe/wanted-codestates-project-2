3530a39d310e258741290bce5bf58414ed87fe39
Kae <kaehehehe@gmail.com>
2022-03-24 15:40:33 +0900

    :lipstick: 버튼 추가

--- src/pages/Ranking/Ranking.jsx
+++ src/pages/Ranking/Ranking.jsx
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import * as S from './style';
+import MatchTypeBtn from '../../components/MatchTypeBtn/MatchTypeBtn';
 
 const Ranking = () => {
-  return <div>Ranking page</div>;
+  const [matchType, setMatchType] = useState('개인전');
+  const [matchCourse, setMatchCourse] = useState('통합');
+
+  return (
+    <S.Container>
+      <S.Buttons>
+        <S.MatchTypeBtn>
+          <MatchTypeBtn
+            matchType={matchType}
+            setMatchType={setMatchType}
+            text_1="개인전"
+            text_2="팀전"
+          />
+        </S.MatchTypeBtn>
+        <MatchTypeBtn
+          matchType={matchCourse}
+          setMatchType={setMatchCourse}
+          text_1="통합"
+          text_2="무한부스터"
+        />
+      </S.Buttons>
+    </S.Container>
+  );
 };
 
 export default Ranking;
---
+++ src/pages/Ranking/style.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 1100px;
+  margin: 0 auto;
+`;
+
+export const Buttons = styled.div`
+  display: flex;
+  padding-left: 10px;
+`;
+
+export const MatchTypeBtn = styled.div`
+  margin-right: 20px;
+`;
