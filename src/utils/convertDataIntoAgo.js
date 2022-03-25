f9d5672c6f15d35693422f3702ef5edc2b2d19f8
Kae <kaehehehe@gmail.com>
2022-03-25 12:25:22 +0900

    :bug: 현재 시간을 기준으로 해서 얼마 전에 플레이한 것인지 구하는 로직 수정

--- src/utils/convertDataIntoAgo.js
+++ src/utils/convertDataIntoAgo.js
@@ -1,38 +1,24 @@
 export const convertDataIntoAgo = (endTime) => {
-  const index = endTime.indexOf('T');
-  const [YEAR, MONTH, DAY] = endTime.slice(0, index).split('-');
-  const [HOUR, MIN, SEC] = endTime.slice(index + 1).split(':');
-
   const today = new Date();
+  const timeValue = new Date(endTime);
 
-  if (today.getFullYear() !== Number(YEAR)) {
-    const year = today.getFullYear() - YEAR;
-    return `${year}년 전`;
+  const difference = Math.floor(
+    (today.getTime() - timeValue.getTime()) / 1000 / 60
+  );
+  if (difference < 1) return '방금전';
+  if (difference < 60) {
+    return `${difference}분전`;
   }
-  if (today.getMonth() + 1 !== Number(MONTH)) {
-    const month = today.getMonth() + 1 - MONTH;
-    return `${month}개원 전`;
+
+  const hour = Math.floor(difference / 60);
+  if (hour < 24) {
+    return `${hour}시간전`;
   }
-  if (today.getDate() !== Number(DAY)) {
-    const day = today.getDate() - DAY;
-    if (DAY >= 7) {
-      const week = Math.floor(DAY / 7);
-      return `${week}주일 전`;
-    } else {
-      return `${day}일 전`;
-    }
+
+  const day = Math.floor(difference / 60 / 24);
+  if (day < 365) {
+    return `${day}일전`;
   }
-  if (today.getHours() !== Number(HOUR)) {
-    const hour = today.getHours() - Number(HOUR);
-    return `${hour}시간 전`;
-  }
-  if (today.getMinutes() !== Number(MIN)) {
-    const min = today.getMinutes() - Number(MIN);
-    return `${min}분 전`;
-  }
-  if (today.getSeconds() !== Number(SEC)) {
-    const sec = today.getSeconds() - Number(SEC);
-    return `${sec}초 전`;
-  }
-  return '지금';
+
+  return `${Math.floor(difference / 365)}년전`;
 };
